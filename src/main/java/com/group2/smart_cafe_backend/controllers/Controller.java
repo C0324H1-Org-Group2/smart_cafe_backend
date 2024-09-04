540592b2af13997b5110a1bc4593eeb7d2ceefd4
tinhnguyen269 <tinhnguyen180507@gmail.com>
2024-09-04 15:10:17 +0700

    test

--- src/main/java/com/group2/smart_cafe_backend/controllers/Controller.java
+++ src/main/java/com/group2/smart_cafe_backend/controllers/Controller.java
@@ -1,4 +1,12 @@
 package com.group2.smart_cafe_backend.controllers;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+@RequestMapping
 public class Controller {
+    @RequestBody
+
+
 }
