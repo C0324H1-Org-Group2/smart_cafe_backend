bcf00b123484757c5c0329884ad84f9f00148c1d
Truong177 <truongtranquang773@gmail.com>
2024-09-09 08:14:50 +0700

    fix login and view profile

---
+++ src/main/java/com/group2/smart_cafe_backend/controllers/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.group2.smart_cafe_backend.controllers;
+
+import com.group2.smart_cafe_backend.models.Employee;
+import com.group2.smart_cafe_backend.services.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    @Autowired
+    private IEmployeeService employeeService;
+    @GetMapping("/{employeeId}")
+    public Employee getNewsById(@PathVariable Long employeeId) {
+        return employeeService.getEmployeeById(employeeId);
+    }
+}
